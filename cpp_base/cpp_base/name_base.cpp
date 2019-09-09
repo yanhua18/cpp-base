821c4d1dd591a9a7d315cdc0fc7a942ead28e73e
yanhua18 <2814050659@qq.com>
2019-09-09 15:30:06 +0800

    内联函数，auto关键字等知识点的复习

--- cpp_base/cpp_base/name_base.cpp
+++ cpp_base/cpp_base/name_base.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
-using namespace std;
-//#include<stdio.h>
+//#include<iostream>
+//using namespace std;
+#include<stdio.h>
 #include<Windows.h>
+#include<time.h>
 /*一个命名空间就定义了一个新的定义域，命名空间中所有的内容都局限于该定义域中*/
 
 
@@ -108,9 +109,10 @@ void TestFunc(int a,int b)
 	cout << "void TestFunc(int)" << endl;
 }
 
-#endif
 
-//如果函数返回时，离开函数作用域后，其栈上空间已经还给系统，因此不能用栈上的空间作为引用类型返回。如果以引用类型返回，返回值的生命周期必须不受函数的限制(即比函数生命周期长)。
+
+//如果函数返回时，离开函数作用域后，其栈上空间已经还给系统，因此不能用栈上的空间作为引用类型返回。如果以引用类型返回，返回值的生命周期必须不受函数的限制(即比函数生命周期长)。
+
 int& Add(int a, int b)
 {
 	int c = a + b;
@@ -123,4 +125,138 @@ int main()
 	cout << "Add(1, 2) is :" << ret << endl;
 	system("pause");
 	return 0;
+}
+
+//传值，传引用，传指针效率比较
+struct A
+{
+	int a[10000];
+};
+void TestFunc1(A a)
+{}
+void TestFunc2(A &a)
+{}
+void TestFunc3(A *a)
+{}
+void TestRefAndValue()
+{
+	A a;
+	//传值
+	size_t begin1 = clock();
+	for (size_t i = 0; i < 10000; ++i)
+	{
+		TestFunc1(a);
+	}
+	size_t end1 = clock();
+	//传引用
+	size_t begin2 = clock();
+	for (size_t i = 0; i < 10000; ++i)
+	{
+		TestFunc2(a);
+	}
+	size_t end2 = clock();
+	//传指针
+	size_t begin3 = clock();
+	for (size_t i = 0; i < 10000; ++i)
+	{
+		TestFunc3(&a);
+	}
+	size_t end3 = clock();
+
+	// 分别计算两个函数运行结束后的时间
+	cout << "TestFunc1(int)-time:" << end1 - begin1 << endl;
+	cout << "TestFunc2(int&)-time:" << end2 - begin2 << endl;
+	cout << "TestFunc2(int*)-time:" << end2 - begin2 << endl;
+
+}
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		TestRefAndValue();
+	}
+	system("pause");
+	return 0;
+}
+
+
+
+struct A
+{
+	int a[10000];
+};
+//做返回值类型
+A a;
+A TestFunc1()
+{
+	return a;
+}
+A& TestFunc2()
+{
+	return a;
+}
+A* TestFunc3()
+{
+	return &a;
+}
+void TestRefAndValue()
+{
+	//返回值
+	size_t begin1 = clock();
+	for (size_t i = 0; i < 10000; ++i)
+	{
+		TestFunc1();
+	}
+	size_t end1 = clock();
+	//返回引用
+	size_t begin2 = clock();
+	for (size_t i = 0; i < 10000; ++i)
+	{
+		TestFunc2();
+	}
+	size_t end2 = clock();
+	//返回指针
+	size_t begin3 = clock();
+	for (size_t i = 0; i < 10000; ++i)
+	{
+		TestFunc3();
+	}
+	size_t end3 = clock();
+
+	// 分别计算两个函数运行结束后的时间
+	cout << "TestFunc1(int)-time:" << end1 - begin1 << endl;
+	cout << "TestFunc2(int&)-time:" << end2 - begin2 << endl;
+	cout << "TestFunc2(int*)-time:" << end2 - begin2 << endl;
+
+}
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		TestRefAndValue();
+	}
+	system("pause");
+	return 0;
+}
+
+#endif
+
+void TestAuto()
+{
+	int a[] = { 1, 2, 3 };
+	auto b[3] = { 4,6 ,7};
 }
+int main()
+{
+	//const int a = 10;
+	//int *pa = (int *)&a;
+	//*pa = 20;
+	//printf("%d", a);//直接以10打印，没有从a空间中取值
+	//printf("%d", *pa);
+
+	TestAuto();
+	system("pause");
+	return 0;
+}
