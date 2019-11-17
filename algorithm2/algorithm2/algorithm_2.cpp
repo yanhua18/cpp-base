e5c98b2df597cd6063f1fb23092b5bf348e43686
yanhua18 <2814050659@qq.com>
2019-11-17 23:27:49 +0800

    牛客网刷题：最长bit位数

--- algorithm2/algorithm2/algorithm_2.cpp
+++ algorithm2/algorithm2/algorithm_2.cpp
@@ -1,8 +1,31 @@
 #include<iostream>
 using namespace std;
+
+
+
+//20191117##########################################################################
+//求最大连续bit数
 int main()
 {
-	printf("hehe\n");
-	system("pause");
-	return 0;
+	int n = 0;
+	while (cin >> n)
+	{
+		int count = 0;
+		int newcount = 0;
+		for (int i = 0; i<32;)
+		{
+			count = 0;
+			while (n >> i & 1)
+			{
+				count++;
+				i++;
+			}
+			i++;
+			if (count>newcount)
+			{
+				newcount = count;
+			}
+		}
+		cout << newcount << endl;
+	}
 }
