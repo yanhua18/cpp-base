7e3157895bf3ee5a6585430e033d207116730859
yanhua18 <2814050659@qq.com>
2019-11-18 16:20:29 +0800

    牛客网刷题：1，二进制插入  2，查找组成一个偶数最接近的两个素数

--- algorithm2/algorithm2/algorithm_2.cpp
+++ algorithm2/algorithm2/algorithm_2.cpp
@@ -3,6 +3,60 @@ using namespace std;
 
 
 
+
+//20191118#########################################################
+//二进制插入
+class BinInsert {
+public:
+	int binInsert(int n, int m, int j, int i) {
+		m <<= j;
+		return n + m;
+	}
+};
+//查找组成一个偶数最接近的两个素数
+bool IsPrime(int x)
+{
+	for (int i = 2; i < x / 2; i++)
+	{
+		if (x%i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	int n = 0;
+	while (cin >> n)
+	{
+		if (n % 2 != 0)
+		{
+			return 0;
+		}
+		int x = 0;
+		int y = 0;
+		//int dest = 0;
+		//int min = 0;
+		for (int i = 2; i <= n / 2; i++)
+		{
+			//dest = n - i - i;
+			if (IsPrime(i) && IsPrime(n - i))
+			{
+				//if (min == 0 || dest>min)
+				//{
+				x = i;
+				y = n - i;
+				//}
+			}
+		}
+		cout << x << endl;
+		cout << y << endl;
+	}
+}
+
+#if 0
 //20191117##########################################################################
 //求最大连续bit数
 int main()
@@ -29,3 +83,4 @@ int main()
 		cout << newcount << endl;
 	}
 }
+#endif
