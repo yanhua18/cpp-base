2fa5d8ccdbb92199cf2f8c0616bdd72a6e1c5781
yanhua18 <2814050659@qq.com>
2019-11-21 22:38:30 +0800

    牛客网刷题：1，幸运的袋子  2，参数解析

--- algorithm2/algorithm2/algorithm_2.cpp
+++ algorithm2/algorithm2/algorithm_2.cpp
@@ -1,9 +1,179 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 using namespace std;
+#include<string>
+#include<string.h>
+#include<vector>
+#include<algorithm>
 
 
+#if 0
+//20191121#################################################################################
+//幸运的袋子
+int n;
+int findall(vector<int> nums, int index, int sum, int mul)
+{
+	int count = 0;
+	for (int i = index; i < n; i++)
+	{
+		sum += nums[i];
+		mul *= nums[i];
+		if (sum > mul)
+		{
+			count++;
+			count += findall(nums, i + 1, sum, mul);
+		}
+		else if (nums[i] == 1)
+		{
+			count += findall(nums, i + 1, sum, mul);
+		}
+		else
+		{
+			break;
+		}
+		sum -= nums[i];
+		mul /= nums[i];
+		while (i < n - 1 && nums[i] == nums[i + 1])
+		{
+			i++;
+		}
+	}
+	return count;
+}
 
+int main()
+{
+	while(cin>>n)
+	{
+		vector<int> x;
+		x.resize(n,0);
+		int mul=1;
+		int sum=0;
+	    int count=0;
+		for(int i=0;i<x.size();i++)
+		{
+			cin>>x[i];
+		}
+		sort(x.begin(),x.end());
+		cout<<findall(x,0,0,1)<<endl;
+	}
+}
 
+int main()
+{
+	int n;
+	while(cin>>n)
+	{
+		vector<int> x;
+		x.resize(n,0);
+		int mul=1;
+		int sum=0;
+		int count=0;
+		for(int i=0;i<x.size();i++)
+		{
+			cin>>x[i];
+		}
+		sort(x.begin(),x.end());
+		for(int i=0;i<x.size();i++)
+		{
+			mul*=x[i];
+			sum+=x[i];
+		}
+		while(mul>sum)
+		{
+			x.pop_back();
+			sum = 0;
+			mul = 1;
+			for(int i=0;i<x.size();i++)
+			{
+				mul*=x[i];
+				sum+=x[i];
+			}
+		}
+		if(x.empty())
+		{
+			cout<<count<<endl;
+			return 0;
+		}
+		else
+		{
+			while (sum >= mul)
+			{
+				count++;
+				x.pop_back();
+				sum = 0;
+				mul = 1;
+				for (int i = 0; i<x.size()&&x.size()>1; i++)
+				{
+					mul *= x[i];
+					sum += x[i];
+				}
+			}
+		}
+		cout<<count<<endl;
+	}
+}
+
+//参数解析***********************
+int main()
+{
+	string str;
+	while (getline(cin, str))
+	{
+		int n = 0;
+		int count = 0;
+		int num = 0;
+		for (int i = 1; i < str.size(); i++)
+		{
+			if (str[i-1] == ' '&&str[i]!=' ')
+			{
+				num++;
+			}
+		}
+		cout << num + 1 << endl;
+		for (int i = 0; i < str.size()+1; i++)
+		{
+			count++;
+			if (str[i] == ' ')
+			{
+					string str1 = str.substr(n, count);
+					cout << str1 << endl;
+					n = i + 1;
+					count = 0;
+			}
+			if (i == str.size())
+			{
+				string str1 = str.substr(n, count-1);
+				cout << str1 << endl;
+			}
+		}
+	}
+}
+
+int main()
+{
+	string str;
+	while(cin>>str)
+	{
+		int num = 0;
+		for (int i = 1; i < str.size(); i++)
+		{
+			if (str[i-1] == ' '&&str[i]!=' ')
+			{
+				num++;
+			}
+		}
+		cout << num + 1 << endl;
+		char * cstr = new char [str.length()+1];
+		strcpy (cstr, str.c_str());
+		char* ptr=strtok(cstr," ");
+		while(ptr!=NULL)
+		{
+			printf("%s\n",ptr);
+			ptr=strtok(NULL," ");
+		}
+	}
+}
 //20191118#########################################################
 //二进制插入
 class BinInsert {
@@ -56,7 +226,7 @@ int main()
 	}
 }
 
-#if 0
+
 //20191117##########################################################################
 //求最大连续bit数
 int main()
