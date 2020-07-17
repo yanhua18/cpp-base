c46f24b43a24ec2019b471b4700926ccbf69aeaf
yanhua18 <2814050659@qq.com>
2020-07-18 00:28:17 +0800

    剑指offer

--- Project1/Project1/1.cpp
+++ Project1/Project1/1.cpp
@@ -944,6 +944,28 @@ public:
 		}
 		return true;
 	}
-
+};
+//39,合并两个排序的链表
+class Solution {
+public:
+	ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+	{
+		if (pHead1 == nullptr)
+			return pHead2;
+		if (pHead2 == nullptr)
+			return pHead1;
+		ListNode* newpHead = nullptr;
+		if (pHead1->val>pHead2->val)
+		{
+			newpHead = pHead2;
+			newpHead->next = Merge(pHead1, pHead2->next);
+		}
+		else
+		{
+			newpHead = pHead1;
+			newpHead->next = Merge(pHead1->next, pHead2);
+		}
+		return newpHead;
+	}
 };
 #endif
