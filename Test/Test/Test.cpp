33e6414107d089efcd9bf55456c3208743d8f3a6
yanhua18 <2814050659@qq.com>
2020-09-11 22:12:35 +0800

    test

--- Test/Test/Test.cpp
+++ Test/Test/Test.cpp
@@ -1,5 +1,38 @@
 #if 0
+#include<iostream>
+using namespace std;
+#include<vector>
 
+int main()
+{
+	int W, L;
+	while (cin >> W >> L)
+	{
+		vector<int> v(L, 0);
+		for (int i = 0; i<L; i++)
+		{
+			cin >> v[i];
+		}
+		int max = v[0], index = 0;;
+		for (int i = 1; i<L; i++)
+		{
+			if (v[i]>max)
+			{
+				max = v[i];
+				index = i;
+			}
+		}
+		int sum = max*W*L;
+		int cap = 0;
+		for (int i = 0; i<L; i++)
+		{
+			cap += v[i] * W;
+		}
+
+		int res = sum - cap;
+		cout << res << endl;
+	}
+}
 #include <stdio.h>
 #include<Windows.h>
 
@@ -31,6 +64,62 @@ int main()
 	int n = 1234;
 	print(n);
 	system("pause");
+	return 0;
+}
+#endif
+
+#if 0
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+int main()
+{
+	vector<int> person;
+	vector<int> risk;
+
+	int val;
+	while (1)
+	{
+		cin >> val;
+		if (getchar() != '\n')
+		{
+			person.push_back(val);
+		}
+		else
+		{
+			break;
+		}
+	}
+	person.push_back(val);
+
+	while (cin >> val)
+	{
+		//cin >> val;
+		if (getchar() != '\n')
+		{
+			risk.push_back(val);
+		}
+		else
+		{
+			break;
+		}
+	}
+	risk.push_back(val);
+
+
+
+	sort(person.begin(), person.end());
+	sort(risk.begin(), risk.end());
+	int num = person.size();
+	for (int i = 0; i<person.size(); i++)
+	{
+		if (person[i]>risk[risk.size() - 1])
+		{
+			num--;
+		}
+	}
+	cout << num << endl;
 	return 0;
 }
 #endif
