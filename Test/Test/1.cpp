880ec1f668a9d5d15d33b974d3afdb62b0d09a61
yanhua18 <2814050659@qq.com>
2020-09-20 21:23:41 +0800

    笔试

--- Test/Test/1.cpp
+++ Test/Test/1.cpp
@@ -5,8 +5,218 @@ using namespace std;
 #include<Windows.h>
 #include<algorithm>
 #include<vector>
-
 #if 0
+int main()
+{
+	string str1;
+	string str2;
+	while (cin >> str1 >> str2)
+	{
+		char arr[256] = { 0 };
+		int count = 0;
+		for (int i = 0; i<str2.size(); ++i)
+		{
+			arr[str2[i]]++;
+			count++;
+		}
+		int sum = count;
+		for (int i = 0; i<str1.size(); ++i)
+		{
+			if (arr[str1[i]]>0)
+			{
+				arr[str1[i]]--;
+				count--;
+			}
+		}
+		cout << sum - count << endl;
+	}
+}
+//Ìæ»»¿Õ¸ñ
+void replaceSpace(char *str, int length) {
+	int count=0;
+	for(int i=0;i<length;++i)
+	{
+		if(str[i]==' ')
+			count++;
+	}
+	int newLen=length+2*count;
+	char* oldl = str + length;
+	char* newl = str + newLen;
+	while (oldl>=str&&newl >= str)
+	{
+		if (*oldl == ' ')
+		{
+			*newl-- = '%';
+			*newl-- = '0';
+			*newl-- = '2';
+			oldl--;
+		}
+		else
+		{
+			*newl-- = *oldl--;
+		}
+	}
+}
+int main()
+{
+	char* str = { "We Are Happy." };
+	int length = 13;
+	replaceSpace(str, length);
+}
+
+
+
+//Ðý×ª×Ö·ûÓÎÏ·
+int main()
+{
+	int m, n;
+	while (cin >> m >> n)
+	{
+		vector<vector<char>> vv(m);
+		for (int i = 0; i<vv.size(); ++i)
+		{
+			vv[i].resize(n, 0);
+		}
+		int count = 0;
+		char temp = 'A';
+		int i = 0, j = 0;
+		int flag = 0;
+		while (count != m*n)
+		{
+			vv[i][j] = temp;
+			count++;
+			temp = count % 26 +'A';
+			if (flag == 0)
+			{
+				if (j + 1<n && vv[i][j + 1] == 0)
+					j++;
+				else
+					flag = 1, i++;
+			}
+			else if (flag == 1)
+			{
+				if (i + 1<m && vv[i + 1][j] == 0)
+					i++;
+				else
+					flag = 2, j--;
+			}
+			else if (flag == 2)
+			{
+				if (j - 1 >= 0 && vv[i][j - 1] == 0)
+					j--;
+				else
+					flag = 3, i--;
+			}
+			else if (flag == 3)
+			{
+				if (i - 1 >= 0 && vv[i - 1][j] == 0)
+					i--;
+				else
+					flag = 0, j++;
+			}
+		}
+		for (int i = 0; i<m; i++)
+		{
+			for (int j = 0; j<n; j++)
+			{
+				cout << " " << vv[i][j];
+			}
+			cout << endl;
+		}
+	}
+}
+#endif
+#if 0
+
+#include<iostream>
+using namespace std;
+#include<string>
+#include<vector>
+int main()
+{
+	string str;
+
+	while (1)
+	{
+		getline(cin, str);
+		vector<string> vv;
+
+		for (int i = 0; i<str.size(); i++)
+		{
+			string s;
+			int flag = 0;
+			while (str[i] >= '0'&&str[i] <= '9')
+			{
+				s += str[i];
+				i++;
+				flag = 1;
+			}
+			if (flag&&(str[i]!='%'))
+				vv.push_back(s);
+		}
+		for (int i = 0; i<vv.size(); ++i)
+		{
+			for (int j = 0; i<vv[i].size(); j++)
+			{
+				if (vv[i][j] == '0')
+					vv[i].erase(j);
+			}
+			int temp = stoi(vv[i]);
+			if (temp >= 1000 && temp <= 3999)
+			{
+				cout << vv[i] << ' ';
+			}
+		}
+		cout << endl;
+		//getchar();
+
+	}
+}
+
+
+
+#include<iostream>
+using namespace std;
+#include<vector>
+vector<vector<int>>  Func(vector<int> v, int n)
+{
+	vector<vector<int>> vv;
+	vector<int> p;
+	int sum = 0;
+	for (int i = 0; i < v.size(); i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			p.push_back(v[j]);
+		}
+		for (int a = 0; a < p.size(); a++)
+		{
+			sum += p[a];
+		}
+		if (sum == n)
+		{
+			vv.push_back(p);
+		}
+	}
+	return vv;
+
+}
+int main()
+{
+	vector<int> v;
+	v.resize(5);
+	for (int i = 0; i < v.size(); i++)
+	{
+		cin >> v[i];
+	}
+	Func(v, 4);
+
+}
+
+
+
+
+
 void Reverse(int m, int n, string& str)
 {
 	int i = m, j = n;
--- Test/Test/Test.cpp
+++ Test/Test/Test.cpp
@@ -1,125 +1,63 @@
-#if 0
 #include<iostream>
 using namespace std;
+#include<string>
 #include<vector>
-
 int main()
 {
-	int W, L;
-	while (cin >> W >> L)
+	int n;
+	while (cin >> n)
 	{
-		vector<int> v(L, 0);
-		for (int i = 0; i<L; i++)
+		vector<string> v;
+		string str;
+		for (int i = 0; i<n; ++i)
 		{
-			cin >> v[i];
+			cin >> str;
+			v.push_back(str);
 		}
-		int max = v[0], index = 0;;
-		for (int i = 1; i<L; i++)
+		for (int i = 0; i<n; ++i)
 		{
-			if (v[i]>max)
+			if (v[i].size()<3)
+				cout << "NO" << endl;
+			else
 			{
-				max = v[i];
-				index = i;
+				string str1 = v[i].substr(0, 3);
+				int flag = 1;
+				for (int j = 3; j<v[i].size();)
+				{
+					int num = 3;
+					int a = 0;
+					int count = 0;
+					while (num && j<v[i].size())
+					{
+						if (v[i][j] == str1[a])
+							count++, a++;
+						num--,j++;
+					}
+					if (count < 2)
+					{
+						flag = 0;
+						break;
+					}
+
+				}
+				if (flag)
+					cout << "Yes" << endl;
+				else
+					cout << "No" << endl;
 			}
 		}
-		int sum = max*W*L;
-		int cap = 0;
-		for (int i = 0; i<L; i++)
-		{
-			cap += v[i] * W;
-		}
-
-		int res = sum - cap;
-		cout << res << endl;
 	}
 }
-#include <stdio.h>
-#include<Windows.h>
-
-
-//编写函数不允许创建临时变量，求字符串的长度
-int Strlen(char* str)
-{
-	if (*str == '\0')
-		return 0;
-	else
-		return 1 + Strlen(str + 1);
-}
-int main()
-{
-	char* str = "abcdef";
-	int len = Strlen(str);
-	printf("%d", len);
-	system("pause");
-}
-//接受一个整型值（无符号），按照顺序打印它的每一位。 例如： 输入：1234，输出 1 2 3 4
-void print(int n)
-{
-	if (n > 9)
-		print(n / 10);
-	printf("%d\n", n%10);
-}
-int main()
-{
-	int n = 1234;
-	print(n);
-	system("pause");
-	return 0;
-}
-#endif
-
-#if 0
-#include<iostream>
-using namespace std;
-#include<vector>
-#include<algorithm>
-int main()
-{
-	vector<int> person;
-	vector<int> risk;
-
-	int val;
-	while (1)
-	{
-		cin >> val;
-		if (getchar() != '\n')
-		{
-			person.push_back(val);
-		}
-		else
-		{
-			break;
-		}
-	}
-	person.push_back(val);
-
-	while (cin >> val)
-	{
-		//cin >> val;
-		if (getchar() != '\n')
-		{
-			risk.push_back(val);
-		}
-		else
-		{
-			break;
-		}
-	}
-	risk.push_back(val);
 
 
 
-	sort(person.begin(), person.end());
-	sort(risk.begin(), risk.end());
-	int num = person.size();
-	for (int i = 0; i<person.size(); i++)
-	{
-		if (person[i]>risk[risk.size() - 1])
-		{
-			num--;
-		}
-	}
-	cout << num << endl;
-	return 0;
-}
-#endif
+
+
+
+
+
+
+
+
+
+
