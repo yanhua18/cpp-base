71aa413f1677c12fbd00a80a7d330670dd1ab18d
yanhua18 <2814050659@qq.com>
2020-09-24 23:37:05 +0800

    笔试

--- Test/Test/1.cpp
+++ Test/Test/1.cpp
@@ -1,11 +1,71 @@
-#include<iostream>
-using namespace std;
-#include<string>
+
 #include<stdio.h>
 #include<Windows.h>
 #include<algorithm>
 #include<vector>
 #if 0
+#include<iostream>
+using namespace std;
+
+
+bool verifyPostorder(int* postorder, int postorderLen) {
+	if (postorderLen == 0)
+		return false;
+	int root = postorder[postorderLen - 1];
+	int i = 0;
+	while (postorder[i]<root)
+		++i;
+	for (int j = i; j<postorderLen - 1; j++)
+	{
+		if (postorder[i] <= root)
+			return false;
+	}
+	bool left = true;
+	if (i>0)
+	{
+		int leftarr[i];
+		for (int n = 0; n<i; ++n)
+		{
+			leftarr[n] = postorder[n];
+		}
+		left = verifyPostorder(leftarr, i);
+	}
+	bool right = true;
+	if (i<postorderLen - 1)
+	{
+		int rightarr[postorderLen - 1 - i];
+		for (int n = 0, m = i; m<postorderLen - 1; ++n, ++m)
+		{
+			rightarr[n] = postorder[m];
+		}
+		right = verifyPostorder(rightarr, postorderLen - i - 1);
+	}
+	return (left&&right);
+}
+
+int main()
+{
+	int arr[5] = { 1, 3, 2, 6, 5 };
+	verifyPostorder(arr, 5);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #include<iostream>
 using namespace std;
--- Test/Test/Test.cpp
+++ Test/Test/Test.cpp
@@ -1,62 +1,31 @@
 #include<iostream>
 using namespace std;
-
-
-bool verifyPostorder(int* postorder, int postorderLen) {
-	if (postorderLen == 0)
-		return false;
-	int root = postorder[postorderLen - 1];
-	int i = 0;
-	while (postorder[i]<root)
-		++i;
-	for (int j = i; j<postorderLen - 1; j++)
-	{
-		if (postorder[i] <= root)
-			return false;
-	}
-	bool left = true;
-	if (i>0)
-	{
-		int leftarr[i];
-		for (int n = 0; n<i; ++n)
-		{
-			leftarr[n] = postorder[n];
-		}
-		left = verifyPostorder(leftarr, i);
-	}
-	bool right = true;
-	if (i<postorderLen - 1)
-	{
-		int rightarr[postorderLen - 1 - i];
-		for (int n = 0, m = i; m<postorderLen - 1; ++n, ++m)
-		{
-			rightarr[n] = postorder[m];
-		}
-		right = verifyPostorder(rightarr, postorderLen - i - 1);
-	}
-	return (left&&right);
-}
-
+#include<string>
 int main()
 {
-	int arr[5] = { 1, 3, 2, 6, 5 };
-	verifyPostorder(arr, 5);
+	string str;
+	while (cin >> str)
+	{
+		
+		string res;
+		for (int i = 0; i<str.size(); ++i)
+		{
+			int count = 1;
+			res += str[i];
+			while (str[i] == str[i + 1])
+			{
+				count++;
+				i++;
+			}
+			res += to_string(count);
+		}
+		if (res.size()<str.size())
+		{
+			cout << res << endl;
+		}
+		else
+		{
+			cout << str << endl;
+		}
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
